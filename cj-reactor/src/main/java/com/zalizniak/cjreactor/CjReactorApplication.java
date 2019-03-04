3fa5c8142538d0197258af1c18f43b503a3b9fca
aillusions <aillusions@gmail.com>
2019-03-04 18:55:40 +0200

    Reactor - wip

---
+++ cj-reactor/src/main/java/com/zalizniak/cjreactor/CjReactorApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.cjreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjReactorApplication.class, args);
+	}
+
+}
