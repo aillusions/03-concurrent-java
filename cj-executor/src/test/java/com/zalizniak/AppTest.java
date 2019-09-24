bee939095c155584b42525140e34165f756c6a0d
alex <alex@zalizniak.com>
2019-09-24 12:26:24 +0300

    Executor service - wip

--- cj-executor/src/test/java/com/zalizniak/AppTest.java
+++ cj-executor/src/test/java/com/zalizniak/AppTest.java
@@ -2,6 +2,7 @@ package com.zalizniak;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,12 +10,32 @@ import java.util.concurrent.Executors;
 public class AppTest {
 
     @Test
-    public void should() {
+    public void should1() {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.submit(() -> {
             System.out.println("I'm from thread: " + Thread.currentThread().getName());
         });
         System.out.println("I'm from thread: " + Thread.currentThread().getName());
+
+        executorService.shutdown();
+    }
+
+    @Test
+    public void should2() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        CountDownLatch countDownLatch = new CountDownLatch(8);
+        Runnable task = () -> {
+            System.out.println("I'm from thread: " + Thread.currentThread().getName());
+            countDownLatch.countDown();
+        };
+
+        for (int i = 0; i < 8; i++) {
+            executorService.submit(task);
+        }
+
+        countDownLatch.await();
+
+        executorService.shutdown();
     }
 
 }
