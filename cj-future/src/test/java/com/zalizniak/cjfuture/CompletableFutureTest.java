4d46f706e5e23ff75aaaac2ac39cba666c548f1c
Oleksandr Zalizniak <oleksandrz@tabtale.com>
2020-07-08 10:03:19 +0300

    CompletableFuture - wip

--- cj-future/src/test/java/com/zalizniak/cjfuture/CompletableFutureTest.java
+++ cj-future/src/test/java/com/zalizniak/cjfuture/CompletableFutureTest.java
@@ -2,13 +2,29 @@ package com.zalizniak.cjfuture;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CompletableFutureTest {
 
+    @Test
+    public void testSupply() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future
+            = CompletableFuture.supplyAsync(() -> "Hello");
+
+        Assert.assertEquals("Hello", future.get());
+    }
+
     public CompletableFuture<String> sayHello(String arg) {
-        return CompletableFuture.completedFuture("hello from future " + arg);
+        // return CompletableFuture.completedFuture("hello from future " + arg);
+
+        return CompletableFuture.supplyAsync(new Supplier<String>() {
+            @Override
+            public String get() {
+                return "hello from future " + arg;
+            }
+        });
     }
 
     @Test
